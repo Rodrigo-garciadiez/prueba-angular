255116a3bfe652d3bb8b226dd7a76167e333e70f
Rodrigo <rgdiez@tcpsi.es>
2021-11-12 12:18:43 +0100

    creacion componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventListComponent } from './events/events-list.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
-    EventListComponent
+    EventListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,25 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'events-list',
-  template:`
-  <div>
-    <h1>Upcoming</h1>
-    <div class="well hoverwell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
-      </div>
+  template: `
+    <div>
+      <h1>Upcoming</h1>
+      <event-thumbnail [event]="event1"></event-thumbnail>
     </div>
-  </div>
   `
+
 })
 export class EventListComponent{
-  event = {
+  event1 = {
     id:1,
     name: 'Angular Connect',
     date: '9/26/2036',
